4b84d30652e769cc2ab6ca6aef3a8007609c2015
Mark Gaspar <48599231+GasparMark@users.noreply.github.com>
2022-04-14 11:23:20 -0700

    328 Lab 1 Coding Question 1

--- Lab1/Lab1.java
+++ Lab1/Lab1.java
@@ -3,7 +3,7 @@ public class Lab1 {
 
     public static void main(String[] args) {
         // write your code here
-        System.out.println("Hi :)");
+        System.out.println("Hi :) ..........");
         
 
     }
