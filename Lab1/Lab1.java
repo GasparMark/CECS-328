882796c2d05c77c0c0ebe15cae674fa46d2cb809
Mark Gaspar <48599231+GasparMark@users.noreply.github.com>
2022-04-17 21:39:33 -0700

    328 Lab 1 Coding Question 1

--- Lab1/Lab1.java
+++ Lab1/Lab1.java
@@ -1,9 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Random;
 
 public class Lab1 {
 
     public static void main(String[] args) {
         // write your code here
-        System.out.println("Hi :) ..........");
+        System.out.println("Hellooooo :) ..........");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a positive number: ");
+
+        int userInt = sc.nextInt();
+
+        System.out.println("The value entered was " + userInt);
+
         
 
     }
